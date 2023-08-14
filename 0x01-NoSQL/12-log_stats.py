3e97851b25597003a0ec68ddf52c466ea79cddad
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-14 18:50:15 +0100

    remove

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -15,17 +15,6 @@ if __name__ == "__main__":
     total_logs = collection.count_documents({})
     print("{} logs".format(total_logs))
     status = collection.count_documents({"path": "/status"})
-    # get_log = collection.count_documents({"method": "GET"})
-    # post_log = collection.count_documents({"method": "POST"})
-    # put_log = collection.count_documents({"method": "PUT"})
-    # patch_log = collection.count_documents({"method": "PATCH"})
-    # delete_log = collection.count_documents({"method": "DELETE"})
-
-    # logs_dict = {"method GET": get_log, "method POST": post_log,
-    #              "method PUT": put_log, "method PATCH": patch_log,
-    #              "method DELETE": delete_log}
-    # for key, value in logs_dict.items():
-    #     print(f"\t{key}: {value}")
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
