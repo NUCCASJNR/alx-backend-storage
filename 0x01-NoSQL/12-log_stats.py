8c7ed05aec957331f9c28c624b7b7723d2d5d733
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-14 18:30:05 +0100

    changed format

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     # Establish the connection
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient(host="localhost", port=27017)
     # Select the database
     db = client["logs"]
     # Select the collection
@@ -15,16 +15,20 @@ if __name__ == "__main__":
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
     status = collection.count_documents({"path": "/status"})
-    get_log = collection.count_documents({"method": "GET"})
-    post_log = collection.count_documents({"method": "POST"})
-    put_log = collection.count_documents({"method": "PUT"})
-    patch_log = collection.count_documents({"method": "PATCH"})
-    delete_log = collection.count_documents({"method": "DELETE"})
+    # get_log = collection.count_documents({"method": "GET"})
+    # post_log = collection.count_documents({"method": "POST"})
+    # put_log = collection.count_documents({"method": "PUT"})
+    # patch_log = collection.count_documents({"method": "PATCH"})
+    # delete_log = collection.count_documents({"method": "DELETE"})
 
-    logs_dict = {"method GET": get_log, "method POST": post_log,
-                 "method PUT": put_log, "method PATCH": patch_log,
-                 "method DELETE": delete_log}
+    # logs_dict = {"method GET": get_log, "method POST": post_log,
+    #              "method PUT": put_log, "method PATCH": patch_log,
+    #              "method DELETE": delete_log}
+    # for key, value in logs_dict.items():
+    #     print(f"\t{key}: {value}")
     print("methods:")
-    for key, value in logs_dict.items():
-        print(f"\t{key}: {value}")
+    methods = ["GET", "POST", "PUT", "DELETE", "PATCH"]
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print(f"\t{method}: {method_count}")
     print(f"{status} status check")
