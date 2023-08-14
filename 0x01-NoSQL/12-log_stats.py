cf86b31641ade3792b7bdc5c571c1235bb1543a3
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-14 17:23:31 +0100

    nginx log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""This module contains a function that lists nginx stats"""
+
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    # Establish the connection
+    client = MongoClient(host="localhost", port=27017)
+    # Select the database
+    db = client["logs"]
+    # Select the collection
+    collection = db.nginx
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+    status = collection.count_documents({"path": "/status"})
+    get_log = collection.count_documents({"method": "GET"})
+    post_log = collection.count_documents({"method": "POST"})
+    put_log = collection.count_documents({"method": "PUT"})
+    patch_log = collection.count_documents({"method": "PATCH"})
+    delete_log = collection.count_documents({"method": "DELETE"})
+
+    logs_dict = {"method GET": get_log, "method POST": post_log,
+                 "method PUT": put_log, "method PATCH": patch_log,
+                 "method DELETE": delete_log}
+    print("methods:")
+    for key, value in logs_dict.items():
+        print(f"\t{key}: {value}")
+    print(f"{status} status check")
