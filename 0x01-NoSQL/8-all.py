dce9a1df68316f4973c3a744cf9b99618e8d469d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-14 14:08:40 +0100

    list all docs

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+a Python function that lists all documents in a collection
+"""
+
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """
+    List all docs in a collection
+    """
+
+    docs = []
+    for doc in mongo_collection.find():
+        docs.append(doc)
+    return docs
