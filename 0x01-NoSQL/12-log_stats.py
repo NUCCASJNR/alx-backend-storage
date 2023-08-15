0cea5a62113f86f415ebca70f2e34c9147c28745
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-15 16:11:09 +0100

    Added annotations to task 12 file

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,20 +4,20 @@
 
 
 from pymongo import MongoClient
-
+from typing import List
 if __name__ == "__main__":
     # Establish the connection
-    client = MongoClient(host="localhost", port=27017)
+    client: MongoClient = MongoClient(host="localhost", port=27017)
     # Select the database
     db = client["logs"]
     # Select the collection
     collection = db.nginx
-    total_logs = collection.count_documents({})
+    total_logs: int = collection.count_documents({})
     print("{} logs".format(total_logs))
-    status = collection.count_documents({"path": "/status"})
+    status: int = collection.count_documents({"path": "/status"})
     print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    methods: List[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        method_count = collection.count_documents({"method": method})
+        method_count: int = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {method_count}")
     print(f"{status} status check")
