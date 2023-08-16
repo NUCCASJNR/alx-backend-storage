7432fadc077d04fecbfdb97cd9a3329bfe7e88fc
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-16 13:32:20 +0100

    true

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,7 +18,7 @@ class Cache:
             redis: Instance of the redis class
         """
         self._redis = Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
